d12c55e748e6eb8265e4535390ac405e1081d59e
Nick White <nick33white@gmail.com>
2023-11-06 10:23:15 -0600

    set IIIF link to Cortex, DC link to IA

--- src/lib/components/image.svelte
+++ src/lib/components/image.svelte
@@ -1,79 +1,81 @@
 <script>
     import { base } from '$app/paths'
-	import { page } from '$app/stores';
-	import Icon from '@iconify/svelte';
-	export let home = false,
-		index = 0;
+    import { page } from '$app/stores';
+    import Icon from '@iconify/svelte';
+    export let home = false,
+    index = 0;
 
-	const homePageImages = [
-		['2KXJ8ZSFRJRJR', 'United States of America', '2KXJ8ZSQ38UEJ', '2KXJ8ZWQ2ML7', 'x997119818805867'],
-		[
-			'2KXJ8ZSFE74FY',
-			'Nationalities map no. 1-4, Polk St. to Twelfth,...Chicago',
-			'2KXJ8ZSQ388AA',
+    const homePageImages = [
+        ['2KXJ8ZSFRJRJR', 'United States of America', '2KXJ8ZSQ38UEJ', '2KXJ8ZWQ2ML7', 'x997119818805867'],
+        [
+            '2KXJ8ZSFE74FY',
+            'Nationalities map no. 1-4, Polk St. to Twelfth,...Chicago',
+            '2KXJ8ZSQ388AA',
             '2KXJ8ZWQNUL5',
-"nby_92356"
-		]
-	];
-	// $: console.log('page.data.minis', $page.data.minis);
-	$: mini = $page.data.mini || false;
-	$: image = mini
-		? [mini.meta.imageiiifid, mini.meta.imagetitle, mini.meta.manifest, mini.meta.imagelink, mini.meta.iaid]
-		: $page.data.minis
+            "nby_92356"
+        ]
+    ];
+    // $: console.log('page.data.minis', $page.data.minis);
+    $: mini = $page.data.mini || false;
+    $: image = mini
+        ? [mini.meta.imageiiifid, mini.meta.imagetitle, mini.meta.manifest, mini.meta.imagelink, mini.meta.iaid]
+        : $page.data.minis
             ? $page.data.minis.map((i) => [i.imageiiifid, i.imagetitle, i.manifest, i.imagelink, i.iaid])[index]
-		: homePageImages[index];
+            : homePageImages[index];
 
-	// $: href = `https://digital.newberry.org/mirador?manifest=https://collections.newberry.org/IIIF3/Presentation/Manifest/${image[2]}`;
-	// $: href = `https://digital.newberry.org/mirador?manifest=https://collections.newberry.org/IIIF3/Presentation/Manifest/${image[2]}`;
+    // $: href = `https://digital.newberry.org/mirador?manifest=https://collections.newberry.org/IIIF3/Presentation/Manifest/${image[2]}`;
+    // $: href = `https://digital.newberry.org/mirador?manifest=https://collections.newberry.org/IIIF3/Presentation/Manifest/${image[2]}`;
 
-    $: iiifurl ="https://iiif.archivelab.org/iiif/" + image[4]
+    $: iiifurl ="https://digital.newberry.org/mirador?manifest=https://collections.newberry.org/IIIF3/Presentation/Manifest/" + image[2]
+    // $: iiifurl ="https://iiif.archivelab.org/iiif/" + image[4]
     $: dcurl ="https://archive.org/details/" + image[4]
-	let tooltipVisible = false;
+    // $: dcurl ="https://collections.newberry.org/asset-management/" + image[2]
+    let tooltipVisible = false;
 
-	function toggleTooltip() {
-		tooltipVisible = !tooltipVisible;
-	}
+    function toggleTooltip() {
+        tooltipVisible = !tooltipVisible;
+    }
 </script>
 
 <div>
 
-            <!-- style="background-image: image-set(url('/600/{ image[0] }_600w.jpg') 600w, url('/1200/{ image[0] }_1200w.jpg') 1200w, url('/2000/{ image[0] }_2000w.jpg') 2000w);" -->
-	<div class="image-container no-lines">
-		<div
-			class="space"
+    <!-- style="background-image: image-set(url('/600/{ image[0] }_600w.jpg') 600w, url('/1200/{ image[0] }_1200w.jpg') 1200w, url('/2000/{ image[0] }_2000w.jpg') 2000w);" -->
+    <div class="image-container no-lines">
+        <div
+            class="space"
             style="background-image:  url('/2000/{image[0]}_2000w.webp');"
-		>
-			{#if tooltipVisible}
-				<div
-					class="tooltip"
-					on:click={() => (tooltipVisible = !tooltipVisible)}
-					on:keydown={() => (tooltipVisible = !tooltipVisible)}
-				>
-					<p>
-						<b>Newberry Digital Collections:</b> view descriptive information, <br />download high
-						resolution images, browse related items
-					</p>
-					<p>
-						<b>IIIF Map Viewer:</b> zoom, compare, and annotate images
-					</p>
-				</div>
-			{/if}
-			<button class="info-icon" aria-label="Jump to gallery" on:click={() => (tooltipVisible = !tooltipVisible)}>
-				<Icon icon="zondicons:question" width="32" color="var(--brand)" />
-			</button>
+            >
+            {#if tooltipVisible}
+                <div
+                    class="tooltip"
+                    on:click={() => (tooltipVisible = !tooltipVisible)}
+                    on:keydown={() => (tooltipVisible = !tooltipVisible)}
+                    >
+                    <p>
+                        <b>Newberry Digital Collections:</b> view descriptive information, <br />download high
+                        resolution images, browse related items
+                    </p>
+                    <p>
+                        <b>IIIF Map Viewer:</b> zoom, compare, and annotate images
+                    </p>
+                </div>
+            {/if}
+            <button class="info-icon" aria-label="Jump to gallery" on:click={() => (tooltipVisible = !tooltipVisible)}>
+                <Icon icon="zondicons:question" width="32" color="var(--brand)" />
+            </button>
             {#if !home}
-<a href="#gallery-anchor" class="jump-tt no-lines" >
-                <div class="jump-tt-text">Jump to essay gallery</div>
-                <Icon icon="emojione-monotone:down-arrow" width="32" color="var(--brand)" />
-            </a>
-{/if}
-		</div>
-    <figcaption class="outer-container">
-        <div class="inner-container">View <em>"</em></div>
-        <div class="inner-container center-container"><em>{image[1]}</em></div>
-        <div class="inner-container"><em>"</em> in a <a href={iiifurl} target="_blank" class="llines bold">IIIF Viewer</a> or in our <a href={dcurl} target="_blank" class="llines bold">Digital Collections</a></div>
-    </figcaption>
-	</div>
+                <a href="#gallery-anchor" class="jump-tt no-lines" >
+                    <div class="jump-tt-text">Jump to essay gallery</div>
+                    <Icon icon="emojione-monotone:down-arrow" width="32" color="var(--brand)" />
+                </a>
+            {/if}
+        </div>
+        <figcaption class="outer-container">
+            <div class="inner-container">View <em>"</em></div>
+            <div class="inner-container center-container"><em>{image[1]}</em></div>
+            <div class="inner-container"><em>"</em> in a <a href={iiifurl} target="_blank" class="llines bold">IIIF Viewer</a> or in our <a href={dcurl} target="_blank" class="llines bold">Digital Collections</a></div>
+        </figcaption>
+    </div>
 </div>
 
 <style>
@@ -100,59 +102,59 @@
     }
     .jump-tt:hover {
 
-		border: 1px solid var(--brand);
-		background: var(--surface-1);
+        border: 1px solid var(--brand);
+        background: var(--surface-1);
     }
     .jump-tt:hover .jump-tt-text {
         opacity: 1;
     }
-	.info-icon {
-		display: block;
-		position: absolute;
-		bottom: 11px;
-		left: 11px;
-		background-color: transparent;
-		border: 1px solid transparent;
-		padding: 4px;
-	}
-	.tooltip {
-		position: absolute;
-		left: 10px;
-		bottom: 10px;
-		border: 1px solid var(--brand);
-		background: var(--surface-1);
-		padding: 8px 8px 4px 44px;
-		/* padding: 8px; */
-	}
-	.tooltip p {
-		margin: 4px;
-	}
-	div,
-	a {
-		position: relative;
-	}
-	.space {
-		position: relative;
-		border: 1px solid var(--brand);
-		flex: 1;
-		width: 100%;
-		display: block;
-		background-size: cover;
-		background-position: center;
-		background-attachment: fixed;
-	}
-	.image-container {
-		/* border: 1px solid var(--brand); */
-		transform: none;
-		height: 60vh;
-		object-fit: cover;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: flex-end;
-		padding: 0;
-		position: relative;
-	}
+    .info-icon {
+        display: block;
+        position: absolute;
+        bottom: 11px;
+        left: 11px;
+        background-color: transparent;
+        border: 1px solid transparent;
+        padding: 4px;
+    }
+    .tooltip {
+        position: absolute;
+        left: 10px;
+        bottom: 10px;
+        border: 1px solid var(--brand);
+        background: var(--surface-1);
+        padding: 8px 8px 4px 44px;
+        /* padding: 8px; */
+    }
+    .tooltip p {
+        margin: 4px;
+    }
+    div,
+    a {
+        position: relative;
+    }
+    .space {
+        position: relative;
+        border: 1px solid var(--brand);
+        flex: 1;
+        width: 100%;
+        display: block;
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+    }
+    .image-container {
+        /* border: 1px solid var(--brand); */
+        transform: none;
+        height: 60vh;
+        object-fit: cover;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-end;
+        padding: 0;
+        position: relative;
+    }
     .bold { 
         font-weight: bold;
     }
--- src/routes/+page.md
+++ src/routes/+page.md
@@ -10,7 +10,7 @@ slug: /
 
 American “Maps of movement” embrace all manner of cartography that enabled or illuminates the historical movements of human beings, goods, and other mappable phenomena across, around, and from the Americas. They are powerful tools for the studying the history of geographical mobility and routemaking as well as economic, social, cultural, intellectual, and political life. From sea charts used by maritime explorers to road maps used by motorists to visit national parks, maps of movement document the detail and pattern of routes; the pace and character of various modes of travel; the imprint of transportation and routes of travel on settlement and migratory patterns; the role of commercial marketing and civic boosterism in geographical movement; and the role mapping and geographical study have played in understanding the earth's surface. American maps of movement have both enabled mobility and shaped conceptions of American landscapes and their possibilities.
 
-_Mapping Movement in American History and Culture_ is a publication of the Newberry Library's Hermon Dunlap Smith Center for the History of Cartography. This online archive and companion to the study, interpretation, and educational use of American maps of movement features more than five hundred high-resolution images of maps drawn from the extensive collections of the Newberry Library dating from the sixteenth to the twenty-first century. The archive is fully searchable by a variety of topics, including mode and context of travel, map format and genre, authors and publishers, geographic area, subject, and date. Accompanying thematic essays and image captions by leading scholars in their fields offer a variety of perspectives on the history and technology of American travel, transportation, commerce, and communications.
+_Mapping Movement in American History and Culture_ is a publication of the Newberry Library's Hermon Dunlap Smith Center for the History of Cartography. This online archive and companion to the study, interpretation, and educational use of American maps of movement features more than five hundred high-resolution images of maps drawn from the extensive collections of the Newberry Library dating from the sixteenth to the twenty-first century. Accompanying thematic essays and image captions by leading scholars in their fields offer a variety of perspectives on the history and technology of American travel, transportation, commerce, and communications.
 
 The production of this site was made possible by a major grant from the Preservation and Access Division of the National Endowment for the Humanities, an independent agency of the United States Government.
 
@@ -24,85 +24,85 @@ All essays in _Mapping Movement in American History and Culture_ were subject to
 
 _Coeditors:_
 
--   James R. Akerman, The Newberry Library (Project Director)
--   Peter Nekola, The Newberry Library
+- James R. Akerman, The Newberry Library (Project Director)
+- Peter Nekola, The Newberry Library
 
 _Project Advisors:_
 
--   Michael P. Conzen, University of Chicago
--   Gerald A. Danzer, University of Illinois at Chicago
--   Ronald Grim, Boston Public Library
--   Jo Guldi, Brown University
--   David Rumsey, Cartography Associates
--   Susan Schulten, University of Denver
+- Michael P. Conzen, University of Chicago
+- Gerald A. Danzer, University of Illinois at Chicago
+- Ronald Grim, Boston Public Library
+- Jo Guldi, Brown University
+- David Rumsey, Cartography Associates
+- Susan Schulten, University of Denver
 
 Many departments of the Newberry Library cooperated in the creation of this resource, including:
 
 _The Newberry Library, Department of Digital Images and Services:_
 
--   Jennifer Thom
--   Catherine Gass
--   John Powell
--   Adam Strohm
+- Jennifer Thom
+- Catherine Gass
+- John Powell
+- Adam Strohm
 
 _The Newberry Library, Department of Collection Services:_
 
--   Linda Ballinger
--   Patrick Morris
--   Jessica Grzegorski
--   Graham Greer
+- Linda Ballinger
+- Patrick Morris
+- Jessica Grzegorski
+- Graham Greer
 
 _The Newberry Library, Editorial Staff:_
 
--   Will Gosner
--   Anne E. Cullen
--   Jarrett P. Dunning
--   Kristin Emery
--   Andrew Epps
--   Douglas Knox
--   Maisie O'Malley
+- Will Gosner
+- Anne E. Cullen
+- Jarrett P. Dunning
+- Kristin Emery
+- Andrew Epps
+- Douglas Knox
+- Maisie O'Malley
 
 _Editorial Assistants:_
 
--   Daniel Greenberg
--   Daniel Jacobs
+- Daniel Greenberg
+- Daniel Jacobs
 
 _The Newberry Library, Department of Conservation Services:_
 
--   Lesa Dowd
--   Linda Kinnaman
--   Barbara Korbel
--   Kaytee Meade
--   Virginia Meredith
--   Becky Saiki
+- Lesa Dowd
+- Linda Kinnaman
+- Barbara Korbel
+- Kaytee Meade
+- Virginia Meredith
+- Becky Saiki
 
 _The Newberry Library, Department of Information Technology:_
 
--   Drin Gyuk
+- Drin Gyuk
 
 This project could not have been undertaken without the support of:
 
--   James Grossman
--   Daniel Greene
--   David Spadafora
--   Diane Dillon
--   D. Bradford Hunt
+- James Grossman
+- Daniel Greene
+- David Spadafora
+- Diane Dillon
+- D. Bradford Hunt
 
 This project also owes much to the tireless work of the Newberry Library Special Collections and General Reading Room Staff.
 
 Additional materials appear in the archive through the courtesy of:
 
--   Arthur and Janet Holzheimer
--   The British Library
--   The Milwaukee Public Library
--   The United States Army Corps of Engineers
+- Arthur and Janet Holzheimer
+- The British Library
+- The Milwaukee Public Library
+- The United States Army Corps of Engineers
 
 The following contributors gifted materials for the project to the Newberry Library:
 
--   James R. Akerman
--   Gerald A. Danzer
--   Ralph E. Ehrenberg
--   Peter Nekola
+- James R. Akerman
+- Gerald A. Danzer
+- Ralph E. Ehrenberg
+- Peter Nekola
 
 ![N.E.H.]({base}/NEH-Preferred-Seal820.jpg)
 
